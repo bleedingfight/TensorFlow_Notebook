087ca83532f01977f8691a735274437f65a8054f
bleedingfight <bleedingfight@126.com>
2017-09-15 12:47:52 +0000

    更新部分错误

--- code/har/lstm_har.py
+++ code/har/lstm_har.py
@@ -1,9 +1,11 @@
+#Deepin 64bit Python3.6.1 TensorFlow1.3 CPU测试通过
 import numpy as np
+import tensorflow as tf
 import os 
 from utilities import *
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-data_path = '/home/liushuai/Picture/UCI HAR Dataset'
+data_path = '/home/liushuai/Pictures/UCI HAR Dataset'
 X_train,labels_train,list_ch_train = read_data(data_path=data_path,split="train")
 X_test,labels_test,list_ch_test = read_data(data_path=data_path,split="test")
 assert list_ch_train == list_ch_test, "Mistmatch in channels!"
@@ -32,17 +34,24 @@ with graph.as_default():
     inputs_ = tf.placeholder(tf.float32,[None,seq_len,n_channels],name = 'inputs')
     labels_ = tf.placeholder(tf.float32,[None,n_classes],name='labels')
     keep_prob_ = tf.placeholder(tf.float32,name = 'keep')
-    learning_rate = tf.placeholder(tf.float32,name = 'learning_rate')
+    learning_rate_ = tf.placeholder(tf.float32,name = 'learning_rate')
 
 with graph.as_default():
-    # 构造LSTM输入
-    lstm_in = tf.transpose(inputs_,[1,0,2])
-    lstm_in = tf.reshape(lstm_in,[-1,n_channels])
-    lstm_in = tf.layers.dense(lstm_in,lstm_size,activation=None)
-    lstm_in = tf.split(lstm_in,seq_len,0)
+    # Construct the LSTM inputs and LSTM cells
+    lstm_in = tf.transpose(inputs_, [1,0,2]) # reshape into (seq_len, N, channels)
+    lstm_in = tf.reshape(lstm_in, [-1, n_channels]) # Now (seq_len*N, n_channels)
+    
+    # To cells
+    lstm_in = tf.layers.dense(lstm_in, lstm_size, activation=None) # or tf.nn.relu, tf.nn.sigmoid, tf.nn.tanh?
+    
+    # Open up the tensor into a list of seq_len pieces
+    lstm_in = tf.split(lstm_in, seq_len, 0)
+    
+    # Add LSTM layers
     lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
-    drop = td.contrib.rnn.MultiRnnCell([Drop]*lstm_layers)
-    initial_state = cell.zeros_state(batch_size,tf.float32)
+    drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_)
+    cell = tf.contrib.rnn.MultiRNNCell([drop] * lstm_layers)
+    initial_state = cell.zero_state(batch_size, tf.float32)
 with graph.as_default():
     outputs,final_state = tf.contrib.rnn.static_rnn(cell,lstm_in,dtype=tf.float32,
             initial_state = initial_state)
@@ -51,10 +60,11 @@ with graph.as_default():
     train_op = tf.train.AdamOptimizer(learning_rate_)
     gradients = train_op.compute_gradients(cost)
     capped_gradients = [(tf.clip_by_value(grad,-1.,1.),var) for grad,var in gradients]
-    corret_pred = tf.equal(tf.argmax(logits,1),tf.argmax(labels_,1))
+    optimizer = train_op.apply_gradients(capped_gradients)
+    correct_pred = tf.equal(tf.argmax(logits,1),tf.argmax(labels_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32),name = 'accuracy')
-if (os.path.exists('checkpoints') == False):
-    os.mkdir('checkpointers')
+if (os.path.exists('checkpoints-rnn') == False):
+    os.mkdir('checkpoints-rnn')
 validation_acc = []
 validation_loss = []
 train_acc = []
@@ -62,11 +72,11 @@ train_loss = []
 with graph.as_default():
     saver = tf.train.Saver()
 with tf.Session(graph=graph) as sess:
-    sess.run(tf.global_variables_initilizer())
+    sess.run(tf.global_variables_initializer())
     iteration = 1
     for e in range(epochs):
         state = sess.run(initial_state)
-        for x,y in get_batch(X_tr,y_tr,batch_size):
+        for x,y in get_batches(X_tr,y_tr,batch_size):
             feed = {inputs_:x,labels_:y,keep_prob_:0.5,
                     initial_state:state,learning_rate_:learning_rate}
             loss,_,state,acc = sess.run([cost,optimizer,final_state,accuracy],feed_dict = feed)
@@ -88,10 +98,72 @@ with tf.Session(graph=graph) as sess:
                     val_loss_.append(loss_v)
                     print("Epoch:{}/{}".format(e,epochs),
                             "Iteration:{:d}".format(iteration),
-                            "Validation loss:{:6f}".format(np.mean(val_loss_))
+                            "Validation loss:{:6f}".format(np.mean(val_loss_)),
                             "Validation acc:{:.6f}".format(np.mean(val_acc_)))
                     validation_acc.append(np.mean(val_acc_))
                     validation_loss.append(np.mean(val_loss_))
                     itertion += 1
                 saver.save(sess,"checkpoints/eeg.ckpt")
 
+with tf.Session(graph=graph) as sess:
+    sess.run(tf.global_variables_initializer())
+    iteration = 1
+    
+    for e in range(epochs):
+        # Initialize 
+        state = sess.run(initial_state)
+        
+        # Loop over batches
+        for x,y in get_batches(X_tr, y_tr, batch_size):
+            
+            # Feed dictionary
+            feed = {inputs_ : x, labels_ : y, keep_prob_ : 0.5, 
+                    initial_state : state, learning_rate_ : learning_rate}
+            
+            loss, _ , state, acc = sess.run([cost, optimizer, final_state, accuracy], 
+                                             feed_dict = feed)
+            train_acc.append(acc)
+            train_loss.append(loss)
+            
+            # Print at each 5 iters
+            if (iteration % 50 == 0):
+                print("Epoch: {}/{}".format(e, epochs),
+                      "Iteration: {:d}".format(iteration),
+                      "Train loss: {:6f}".format(loss),
+                      "Train acc: {:.6f}".format(acc))
+            
+            # Compute validation loss at every 25 iterations
+            if (iteration%100 == 0):
+                
+                # Initiate for validation set
+                val_state = sess.run(cell.zero_state(batch_size, tf.float32))
+                
+                val_acc_ = []
+                val_loss_ = []
+                for x_v, y_v in get_batches(X_vld, y_vld, batch_size):
+                    # Feed
+                    feed = {inputs_ : x_v, labels_ : y_v, keep_prob_ : 1.0, initial_state : val_state}
+                    
+                    # Loss
+                    loss_v, state_v, acc_v = sess.run([cost, final_state, accuracy], feed_dict = feed)
+                    
+                    val_acc_.append(acc_v)
+                    val_loss_.append(loss_v)
+                
+                # Print info
+                print("Epoch: {}/{}".format(e, epochs),
+                      "Iteration: {:d}".format(iteration),
+                      "Validation loss: {:6f}".format(np.mean(val_loss_)),
+                      "Validation acc: {:.6f}".format(np.mean(val_acc_)))
+                
+                # Store
+                validation_acc.append(np.mean(val_acc_))
+                validation_loss.append(np.mean(val_loss_))
+            
+            # Iterate 
+            iteration += 1
+    
+    saver.save(sess,"checkpoints/eeg.ckpt")
+plt.plot(validation_acc,validation_loss,'r')
+plt.title('accuracy with loss')
+plt.savefig('acc_loss.png',dpi=800)
